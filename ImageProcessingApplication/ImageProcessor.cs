79433f8825eedcc745aeed08f1eb19ccf7408e8c
Ekaterina Sazdova <74919977+SazdovaEkaterina@users.noreply.github.com>
2023-07-11 14:31:01 +0200

    change images brightness

--- ImageProcessingApplication/ImageProcessor.cs
+++ ImageProcessingApplication/ImageProcessor.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using System.Text;
 
 namespace ImageProcessingApplication;
 
@@ -34,9 +35,73 @@ public class ImageProcessor
         }
     }
 
-    public async Task SetBrightness()
+    private async Task SetBrightnessGroup(List<Image> imagesGroup)
     {
+        foreach (var image in imagesGroup)
+        {
+            await Task.Delay(2000);
+            switch (image.Filter)
+            {
+                case Filter.SEPIA:
+                    image.Brightness = -5;
+                    break;
+                case Filter.BLUR:
+                    image.Brightness = 0;
+                    break;
+                case Filter.GRAYSCALE:
+                    image.Brightness = 10;
+                    break;
+            }
+            Console.WriteLine($"The brightness of image {image.Name} has been set to {image.Brightness}.");
+        }
+    }
+
+    public async Task SetBrightnessAll()
+    {
+        List<Image> sepiaImages = new List<Image>();
+        List<Image> blurImages = new List<Image>();
+        List<Image> grayscaleImages = new List<Image>();
         
+        foreach (var image in Images)
+        {
+            switch (image.Filter)
+            {
+                case Filter.SEPIA:
+                    sepiaImages.Add(image);
+                    break;
+                case Filter.BLUR:
+                    blurImages.Add(image);
+                    break;
+                case Filter.GRAYSCALE:
+                    grayscaleImages.Add(image);
+                    break;
+            }
+        }
+        
+        //printing info an all the groups
+        Console.WriteLine($"Sepia images: {DisplayGroup(sepiaImages)}");
+        Console.WriteLine($"Blur images: {DisplayGroup(blurImages)}");
+        Console.WriteLine($"Grayscale images: {DisplayGroup(grayscaleImages)}");
+
+        var tasks = new List<Task>();
+        //The groups can run at the same time:
+        tasks.Add(SetBrightnessGroup(sepiaImages));
+        tasks.Add(SetBrightnessGroup(blurImages));
+        tasks.Add(SetBrightnessGroup(grayscaleImages));
+        await Task.WhenAll(tasks);
+        
+        Console.WriteLine("The entire task is complete");
+        
+    }
+
+    public string DisplayGroup(List<Image> images)
+    {
+        var stringBuilder = new StringBuilder();
+        foreach (var image in images)
+        {
+            stringBuilder.Append($"{image.Name} ");
+        }
+        return stringBuilder.ToString();
     }
 
 }
