6461da5d53f4c3d5153aca10c4a6f7530f11f85f
Ekaterina Sazdova <74919977+SazdovaEkaterina@users.noreply.github.com>
2023-07-12 11:53:49 +0200

    change project name

---
+++ DataProcessingApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task2AsyncExercise0;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        string inputFilePath = "/Users/ekaterinasazdova/Documents/GitHub/practice-tasks/DataProcessingApplication/input.txt";
+        string outputFilePath = "/Users/ekaterinasazdova/Documents/GitHub/practice-tasks/DataProcessingApplication/output.txt";
+
+        DataProcessor dataProcessor = new DataProcessor(inputFilePath, outputFilePath);
+        await dataProcessor.ReadFile();
+        
+    }
+}
